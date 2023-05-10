d56ebca8317d45627a210d444be66825e9158519
Megan Beyl <231206@virtualwindow.co.za>
2023-05-10 15:30:32 +0200

    Class done

---
+++ js /index.js
@@ -0,0 +1,17 @@
+let str = "";
+
+if(str === ""){
+    console.log("String is Empty")
+} else {
+    console.log("String is NOT Empty")
+}
+
+let value = 10;
+
+for(let i = 0; i <= value; i++){
+    console.log(i)
+}
+
+for(let i = value; i >= 0; i--){
+    console.log(i)
+}
